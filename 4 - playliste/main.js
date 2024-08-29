ca4efb34acbd8a60833c755f0e02379cd8e203c1
Lasse <laje0005@stud.kea.dk>
2024-08-29 10:29:22 +0200

    Øvelse playliste

---
+++ 4 - playliste/main.js
@@ -0,0 +1,12 @@
+
+
+     const playliste = document.querySelector("ul");
+
+     const playListe = ["Noget med Danser med Drenge", "Noget med Gavin DeGraw", "Noget med MJ", "Noget med Biebs", "Noget med Destinys Child", "Noget med Backstreet Boys", "Noget med Alicia Keys", "Noget med Adele", "Noget med Osmo Ikonen"];
+
+     playListe.forEach(putListe);
+
+     function putListe(element) {
+       console.log(element);
+       playliste.innerHTML += "<li>" + element + "</li>";
+     }
